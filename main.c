b405bffb875e1c3a18f861927e335c64ecd9f7c8
elfadili-ae <elfadili.ae@gmail.com>
2023-05-10 10:45:43 +0000

    README.md file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * main - a simple shell
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	
+	return (0);
+}
